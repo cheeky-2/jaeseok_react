1045ac7f3c873dc6a8cea2515a188a1ebb33b940
cheeky-2 <jjuing0@gmail.com>
2025-07-07 12:01:17 +0900

    build

---
+++ src/components/Intro.jsx
@@ -0,0 +1,15 @@
+import jaeseok2 from '../assets/images/jaeseok2.png';
+function Intro(){
+    return(<>
+        <h1>소개</h1>
+        <div class="contents">
+            <p>1972년 8월 14일</p>
+            <p>서울특별시 성북구 수유동 출생</p>
+            <p>국적 : 대한민국 / 종교 : 불교</p>
+            <p>178cm, 65kg, B형, 270mm, ISFP</p>
+            <p>서울예술전문대학 방송연예과 중퇴</p>
+        </div>
+        <p><img src={jaeseok2} alt="" /></p>
+    </>)
+}
+export default Intro;
